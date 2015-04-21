42ed2e4ee3eee68ff16aee1f04b920ce6d636949
Eloy Espinaco <eloyesp@gmail.com>
2015-04-21 11:48:56 -0300

    Leave my comments.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -32,6 +32,7 @@ setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
+setopt INTERACTIVECOMMENTS # ignore comments
 
 zle -N newtab
 
